b537ffccea8d6634066d0e1847d89cfbf019556d
smotz9 <SamanthaMotz9@gmail.com>
2022-11-25 19:36:16 -0500

    first commit

---
+++ EAS4610_FinalProject.py
@@ -0,0 +1,21 @@
+##%%----- 1D  Richards'  Equation -----%%##
+##---- Samantha Motz -- Leah Hornsey ----##
+##--------- December  6th, 2022 ---------##
+
+#%%----- Parameters -----%%#
+
+#%%----- Functions  -----%%#
+
+#%%----- Model Grid -----%%#
+
+#%%----- Time steps -----%%#
+
+#%%-- Pre-Allocations  --%%#
+
+#%%---- Initial Soln ----%%#
+
+#%%------ Matrices ------%%#
+
+#%%------ Plotting ------%%#
+
+#%%---- Project Link ----%%#
