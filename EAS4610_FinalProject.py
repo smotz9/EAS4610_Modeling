70f1786c52d24ed6179f4c2f218a2717796ecb86
smotz9 <SamanthaMotz9@gmail.com>
2022-11-26 19:17:13 -0500

    first commit

--- EAS4610_FinalProject.py
+++ EAS4610_FinalProject.py
@@ -1,7 +1,9 @@
 ##%%----- 1D  Richards'  Equation -----%%##
 ##---- Samantha Motz -- Leah Hornsey ----##
 ##--------- December  6th, 2022 ---------##
-
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import spdiags
 #%%----- Parameters -----%%#
 
 #%%----- Functions  -----%%#
